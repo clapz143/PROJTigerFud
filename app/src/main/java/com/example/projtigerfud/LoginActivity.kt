2b1843f9b0875d3c2205d5e4e6cfa18c62620fb8
t0hyang <kirstenfaye.zulaybar.cics@ust.edu.ph>
2023-11-30 19:33:20 +0800

    updated all login signup and category

--- app/src/main/java/com/example/projtigerfud/LoginActivity.kt
+++ app/src/main/java/com/example/projtigerfud/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.projtigerfud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.projtigerfud.databinding.ActivityLoginBinding
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
@@ -22,19 +25,52 @@ class LoginActivity : AppCompatActivity() {
         firebaseDatabase = FirebaseDatabase.getInstance()
         databaseReference = firebaseDatabase.reference.child("users")
 
+        binding.LoginButton.setOnClickListener {
+            // username and password that user will write the details we will take them with the help of text.toString
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+
+            // pag dalawa is not empty ang username and password, i call this signup user method which created before sa loob neto it will pass signupUsername, signupPassword
+            if(loginUsername.isNotEmpty() && loginPassword.isNotEmpty()){
+                loginUser(loginUsername, loginPassword)
+            }else { // pag yung user black lang nilagay it will toast as all fields are mandatory
+                Toast.makeText(this@LoginActivity, "All Fields are mandatory", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.signupRedirect.setOnClickListener{
+            startActivity(Intent(this@LoginActivity, SignupActivity::class.java))
+            finish()
+        }
+
     }
 
-//    private fun loginUser(username: String, password: String){
-//        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object: ValueEventListener{
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                if (dataSnapshot.exists()){
-//                    for (userSnapshot in dataSnapshot.children){
-//                        val userData = userSnapshot = userSnapshot.getValue(UserData::class.java)
-//
-//                    }
-//
-//                }
-//            }
-//        })
-//    }
+    private fun loginUser(username: String, password: String){
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (userSnapshot in dataSnapshot.children){ // it breaks through children of data snapshot children as in username and password and ID
+                        val userData = userSnapshot.getValue(UserData::class.java)// this line snapshot will take all the values form the user data class basically user data consists of username password and ID
+
+                        if (userData != null && userData.password == password){ // here is checking validation
+                            Toast.makeText(this@LoginActivity, "Login Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@LoginActivity, MainCategory::class.java))
+                            finish()
+                            return
+
+                        }
+
+                    }
+
+                }
+                Toast.makeText(this@LoginActivity, "Login Failed, Please Try Again", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@LoginActivity, "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
+    }
 }
